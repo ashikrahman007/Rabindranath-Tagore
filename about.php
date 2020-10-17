f5d6219fe620af2227bcb5fc4a967d03bb17c074
ashikrahman007 <53301000+ashikrahman007@users.noreply.github.com>
2020-10-18 00:36:03 +0600

    Rabindranath Tagore
    MY First Projects!!!

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>
+      Super shop management System
+   </title>
+</head>
+<body style="background-image:url('ashik.jpg');background-size:1500px 625px;">
+   <h1 style="font-family:verdana;text-align:center;color:orange">Rabindranath General store </h1>
+
+<p style="font-family:courier;text-align:center;color:gold"> <q>This is a super shop where all product are found</q>
+</P>
+<p style="color:gold;font-family:courier;text-align:center;"> you can buy any product from our site </q></p>
+
+
+       <p style="font-family:courier;text-align:center;color:gold;font-family:fantasy;">contact info:</p>
+     
+       <p style="text-align:center;color:gold;font-family:fantasy;"> 01752-324339</p>
+       <p style="text-align:center;color:gold"> <b> <i>  E-mail:ashikrahaman2011@gmail.com </b> </i></p>
+      <p style="text-align:center;font-family:cursive;"> For more information visit our major website </p>
+      
+<p style="text-align: center"><a href="https://www.daraz.com.bd/">Click here</a></p>
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body style="background-image:url('ashik.jpg');background-size:1500px 625px;">
+	<title>
+      log In
+   </title>
+
+<h2 style="color:red">Log-in</h2>
+
+<form action="/action_page.php">
+	
+	
+  user name:<br>
+  <input type="text" name="firstname" value="">
+  <br>
+  password:<br>
+  <input type="varchar" name="lastname" value="">
+  <br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+
+</body>
+</html>
